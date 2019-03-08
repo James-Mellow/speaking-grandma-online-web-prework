a66ec21baf3e114e9a1469127d4856de9be905a2
Matthew Muldrow <jmatthewm92@gmail.com>
2019-03-08 00:24:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,10 @@ def speak_to_grandma
     puts "HUH?! SPEAK UP, SONNY!"
   if else "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
-  if else ""
+  if else "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1986!"
+  if else "I LOVE YOU GRANDMA!"
+    puts I LOVE YOU TOO PUMPKIN!""
 
 
 
